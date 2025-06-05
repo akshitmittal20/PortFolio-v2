d3365dcc6bfcd0efb0cd41542e2a6f35e6339b2e
akshitmittal20 <akshitmittal20@gmail.com>
2025-06-05 12:12:47 +0530

    v

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'akshitmittal20@gmail.com',
-    phone: '+91-9953894030',
+    // phone: '+91-9953894030',
     address: 'Rohini, Delhi-110086, India ',
     github: 'https://github.com/akshitmittal20',
     linkedIn: 'https://linkedin.com/in/akshit-mittal-410893162/'
