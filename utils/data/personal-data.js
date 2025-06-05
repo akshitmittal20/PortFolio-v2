65106032a3110ffbe499af1731d4711884001875
akshitmittal20 <akshitmittal20@gmail.com>
2025-06-05 12:55:28 +0530

    resume

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -9,5 +9,5 @@ export const personalData = {
   github: 'https://github.com/akshitmittal20',  
   linkedIn: 'https://linkedin.com/in/akshit-mittal-410893162/',    
   leetcode: "https://leetcode.com/akshitmittal20",
-  resume: "https://drive.google.com/file/d/12osEh2YDVeKfNOvqjB466szSCd4cTkel/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1LgdCsVsqJcOiZHl3-ZbHFZTXKAmxQPpM/view?usp=sharing"
 }
