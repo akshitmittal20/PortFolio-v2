ff4fedc84c1d716c48caa6dfe50ddaba59963e0c
akshitmittal20 <akshitmittal20@gmail.com>
2025-10-07 20:25:43 +0530

    done

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -9,5 +9,5 @@ export const personalData = {
   github: 'https://github.com/akshitmittal20',  
   linkedIn: 'https://linkedin.com/in/akshit-mittal-410893162/',    
   leetcode: "https://leetcode.com/akshitmittal20",
-  resume: "https://drive.google.com/file/d/1HgrodMFm5pr_F3MlbYS1kiFQU4UXS7Sw/view?usp=drive_link"
+  resume: "https://drive.google.com/file/d/1AdvmE_nVO8sIMb01W0k2wVxXnNPxlocP/view?usp=drive_link"
 }
